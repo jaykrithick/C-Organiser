79b1d2ce87826e4d66e5acd21a105c67683bbf06
jaykrithick <jaikrithick509@gmail.com>
2023-10-25 21:26:04 +0530

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileOrganiser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string Path;
+        public bool organiserStatus = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MinimiseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void SetDownloadsDirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Path = PathInput.Text;
+            PathInput.Text = Path;
+        }
+        private void enableDisableBtn_Click(object sender, RoutedEventArgs e)
+        {
+            organiserStatus = !organiserStatus;
+            if (organiserStatus == true)
+            {
+                organiserStatusTxt.Text = "Organiser Enabled";
+            }
+            else
+            {
+                organiserStatusTxt.Text = "Organiser Disabled";
+            }
+        }
+        
+    }
+}
