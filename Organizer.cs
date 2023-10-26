309e9b25f8fc52159c25990ac06f4e91325d0ce9
jaykrithick <jaikrithick509@gmail.com>
2023-10-26 22:03:46 +0530

    Service Created
    Service created window hides instead of shutting down the app

---
+++ Organizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace FileOrganiser
+{
+    public class Organizer
+    {
+
+        private readonly Timer _timer;
+
+        public Organizer()
+        {
+            _timer = new Timer(1000);
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            
+        }
+    }
+}
